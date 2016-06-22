46f313cd7ae4532628eea4ffd6ece2e5a92c27ea
KalsCode <anthonykalanda@gmail.com>
2016-06-22 14:06:57 +0300

    Initial project set up

---
+++ Libray/LibForm.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Libray
+{
+    partial class LibForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LblBkcode = new System.Windows.Forms.Label();
+            this.LblStudId = new System.Windows.Forms.Label();
+            this.txtStudId = new System.Windows.Forms.TextBox();
+            this.txtBkCode = new System.Windows.Forms.TextBox();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.libraryDataSet = new Libray.LibraryDataSet();
+            this.libUserBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.libUserTableAdapter = new Libray.LibraryDataSetTableAdapters.LibUserTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bookCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remarksDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LblStatus = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libUserBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblBkcode
+            // 
+            this.LblBkcode.AutoSize = true;
+            this.LblBkcode.Location = new System.Drawing.Point(12, 9);
+            this.LblBkcode.Name = "LblBkcode";
+            this.LblBkcode.Size = new System.Drawing.Size(60, 13);
+            this.LblBkcode.TabIndex = 1;
+            this.LblBkcode.Text = "Book Code";
+            // 
+            // LblStudId
+            // 
+            this.LblStudId.AutoSize = true;
+            this.LblStudId.Location = new System.Drawing.Point(244, 9);
+            this.LblStudId.Name = "LblStudId";
+            this.LblStudId.Size = new System.Drawing.Size(56, 13);
+            this.LblStudId.TabIndex = 2;
+            this.LblStudId.Text = "Student Id";
+            // 
+            // txtStudId
+            // 
+            this.txtStudId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStudId.Location = new System.Drawing.Point(247, 25);
+            this.txtStudId.Name = "txtStudId";
+            this.txtStudId.Size = new System.Drawing.Size(252, 26);
+            this.txtStudId.TabIndex = 3;
+            // 
+            // txtBkCode
+            // 
+            this.txtBkCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBkCode.Location = new System.Drawing.Point(12, 25);
+            this.txtBkCode.Name = "txtBkCode";
+            this.txtBkCode.Size = new System.Drawing.Size(206, 26);
+            this.txtBkCode.TabIndex = 4;
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.Location = new System.Drawing.Point(568, 266);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(75, 23);
+            this.BtnRemove.TabIndex = 5;
+            this.BtnRemove.Text = "Remove";
+            this.BtnRemove.UseVisualStyleBackColor = true;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(568, 163);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(75, 23);
+            this.BtnAdd.TabIndex = 6;
+            this.BtnAdd.Text = "Add";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.dateDataGridViewTextBoxColumn,
+            this.bookCodeDataGridViewTextBoxColumn,
+            this.studentIdDataGridViewTextBoxColumn,
+            this.remarksDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.libUserBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(539, 378);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // libraryDataSet
+            // 
+            this.libraryDataSet.DataSetName = "LibraryDataSet";
+            this.libraryDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // libUserBindingSource
+            // 
+            this.libUserBindingSource.DataMember = "LibUser";
+            this.libUserBindingSource.DataSource = this.libraryDataSet;
+            // 
+            // libUserTableAdapter
+            // 
+            this.libUserTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // dateDataGridViewTextBoxColumn
+            // 
+            this.dateDataGridViewTextBoxColumn.DataPropertyName = "Date";
+            this.dateDataGridViewTextBoxColumn.HeaderText = "Date";
+            this.dateDataGridViewTextBoxColumn.Name = "dateDataGridViewTextBoxColumn";
+            // 
+            // bookCodeDataGridViewTextBoxColumn
+            // 
+            this.bookCodeDataGridViewTextBoxColumn.DataPropertyName = "BookCode";
+            this.bookCodeDataGridViewTextBoxColumn.HeaderText = "BookCode";
+            this.bookCodeDataGridViewTextBoxColumn.Name = "bookCodeDataGridViewTextBoxColumn";
+            // 
+            // studentIdDataGridViewTextBoxColumn
+            // 
+            this.studentIdDataGridViewTextBoxColumn.DataPropertyName = "StudentId";
+            this.studentIdDataGridViewTextBoxColumn.HeaderText = "StudentId";
+            this.studentIdDataGridViewTextBoxColumn.Name = "studentIdDataGridViewTextBoxColumn";
+            // 
+            // remarksDataGridViewTextBoxColumn
+            // 
+            this.remarksDataGridViewTextBoxColumn.DataPropertyName = "Remarks";
+            this.remarksDataGridViewTextBoxColumn.HeaderText = "Remarks";
+            this.remarksDataGridViewTextBoxColumn.Name = "remarksDataGridViewTextBoxColumn";
+            // 
+            // LblStatus
+            // 
+            this.LblStatus.AutoSize = true;
+            this.LblStatus.Location = new System.Drawing.Point(12, 449);
+            this.LblStatus.Name = "LblStatus";
+            this.LblStatus.Size = new System.Drawing.Size(0, 13);
+            this.LblStatus.TabIndex = 8;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(548, 3);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.TabIndex = 9;
+            this.lblDate.Text = "Date";
+            // 
+            // LibForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 471);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.LblStatus);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.txtBkCode);
+            this.Controls.Add(this.txtStudId);
+            this.Controls.Add(this.LblStudId);
+            this.Controls.Add(this.LblBkcode);
+            this.Name = "LibForm";
+            this.Text = "Library App";
+            this.Load += new System.EventHandler(this.LibForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libUserBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LblBkcode;
+        private System.Windows.Forms.Label LblStudId;
+        private System.Windows.Forms.TextBox txtStudId;
+        private System.Windows.Forms.TextBox txtBkCode;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.Button BtnAdd;
+        private LibraryDataSet libraryDataSet;
+        private LibraryDataSetTableAdapters.LibUserTableAdapter libUserTableAdapter;
+        private System.Windows.Forms.BindingSource libUserBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn remarksDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label LblStatus;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bookCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Libray/LibForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Libray;
+
+namespace Libray
+{
+    public partial class LibForm : Form
+    {
+        
+        public LibForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LibForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'libraryDataSet.LibUser' table. You can move, or remove it, as needed.
+            this.libUserTableAdapter.Fill(this.libraryDataSet.LibUser);
+            // TODO: This line of code loads data into the 'libDatabaseDataSet.LibUser' table. You can move, or remove it, as needed.
+            this.lblDate.Text = DateTime.Today.ToLongDateString();
+
+        }
+    }
+}
